8a04280f01572a6ed78eb32314787e58cf533408
Vibol T <vibolteav@gmail.com>
2021-05-15 02:21:49 +0700

    fixed auto load

--- lib/epp-client.rb
+++ lib/epp-client.rb
@@ -24,6 +24,7 @@ module EPP
   end
 
   module Commands
+    autoload :AcceptTransfer,    File.expand_path('../epp-client/commands/accept_transfer.rb',    __FILE__)
     autoload :Check,    File.expand_path('../epp-client/commands/check.rb',    __FILE__)
     autoload :Create,   File.expand_path('../epp-client/commands/create.rb',   __FILE__)
     autoload :Delete,   File.expand_path('../epp-client/commands/delete.rb',   __FILE__)
